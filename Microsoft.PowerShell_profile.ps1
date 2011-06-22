cc149c335926a252f422585a8db14dc9b9157680
ang3lfir3 <ang3lfir3@gmail.com>
2011-06-22 16:45:48 -0700

    updating my profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,19 +1,22 @@
-$env:Path += ";c:\scripts;c:\tools"
+Import-Module posh-git
+Import-Module Chewie
+
+$env:Path += ";c:\scripts;c:\tools;c:\tools\perforce"
 Set-Location D:\projects
 
+
 function prompt {
-
-$path = (Get-Location).Path
-
-    if ($path.Length -gt 45)
-    {
-        $path = Get-Prompt($path);
-    }    
-    "$path>"
+	Write-Host(Get-Prompt($pwd)) -nonewline
+	$Global:GitStatus = Get-GitStatus
+    Write-GitStatus $GitStatus
+	
+    ">"
 }
 
 function Get-Prompt([string] $path)
 {
+	if ($path.Length -lt 35) { return $path}
+	
     $loc = "";
     $tokens = $path.Split('\');
     
@@ -21,15 +24,36 @@ function Get-Prompt([string] $path)
     {
         $loc += $tokens[$i][0] + '\';
     }
-    
+    $loc = $loc.ToUpper()
     $loc += $tokens[$tokens.count -1]
        
-    return $loc.ToUpper();
+    return $loc;
+}
+
+#aliases
+New-Alias -name 'np' -value 'C:\Program Files (x86)\Notepad++\notepad++.exe'
+
+#posh git stuffs
+if(-not (Test-Path Function:\DefaultTabExpansion)) {
+    Rename-Item Function:\TabExpansion DefaultTabExpansion
+}
+# Set up tab expansion and include git expansion
+function TabExpansion($line, $lastWord) {
+    $lastBlock = [regex]::Split($line, '[|;]')[-1]
+    
+    switch -regex ($lastBlock) {
+        # Execute git tab completion for all git-related commands
+        'git (.*)' { GitTabExpansion $lastBlock }
+        # Fall back on existing tab expansion
+        default { DefaultTabExpansion $line $lastWord }
+    }
 }
 
 function touch([string] $path) 
 {	$test = test-path($path)
 	if(!$test) {new-item -path $path -type file}
+	
+	np $path
 }
 
 function SetHttpProxyEnv($proxy)
@@ -37,4 +61,3 @@ function SetHttpProxyEnv($proxy)
 	$env:HTTP_PROXY = $proxy
 	$env:HTTPS_Proxy = $proxy
 }
-New-Alias -name 'np' -value 'C:\Program Files (x86)\Notepad++\notepad++.exe'
